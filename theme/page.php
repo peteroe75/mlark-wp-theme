44e29d856fd38b83c82646e0c3c9927a770713a5
Peter Roe <89943260+peteroe75@users.noreply.github.com>
2026-02-05 12:01:55 -0500

    Update page.php

--- theme/page.php
+++ theme/page.php
@@ -1,12 +1,16 @@
 <?php get_header(); ?>
 
 <main id="content">
-  <?php
+<div class="mlark-wrap">
+
+
+        <?php
   while (have_posts()) {
     the_post();
     the_content();
   }
   ?>
+        </div>
 </main>
 
 <?php get_footer(); ?>
