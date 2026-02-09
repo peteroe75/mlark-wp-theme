fca509f33bafaec6aeb28e9af6821c919342a699
Peter Roe <89943260+peteroe75@users.noreply.github.com>
2026-02-09 10:16:21 -0500

    Update header.php
    .

--- theme/header.php
+++ theme/header.php
@@ -4,6 +4,10 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+
+<!---PUT HEADER CODE HERE --->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600;700&display=swap">
+
 </head>
 <body <?php body_class(); ?>>
 
