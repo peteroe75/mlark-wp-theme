f4a1430b38629840ee67a8debf2e5eb56a32b211
Peter Roe <89943260+peteroe75@users.noreply.github.com>
2026-02-16 09:59:23 -0500

    Update single.php
    .

--- theme/single.php
+++ theme/single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main id="content">
-
+<div class="mlark-wrap">
   <?php
   while (have_posts()) {
     the_post();
@@ -14,7 +14,7 @@
     <?php
   }
   ?>
-
+</div>
 </main>
 
 <?php get_footer(); ?>
